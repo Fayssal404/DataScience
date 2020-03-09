b00f5e67e321590e7e45012518ef77790eea4146
fayssal404 <fayssal.ds.stat@gmail.com>
2020-03-09 16:44:47 +0100

    Documentation & Introducing recursivety

--- neural-network/backpropagation/nn-backproba-demo1.py
+++ neural-network/backpropagation/nn-backproba-demo1.py
@@ -1,19 +1,20 @@
+
+import numpy as np
 class NeuralNetwork:
     
     def __init__(self):
-        # ANN; Input layer (2x1) - L1 = (2x1) - L2 = (3x1) 
-        self.input_size = 2
-        self.output_size = 1
-        self.hidden_size = 3
+        self.input_size = 2 # Fixed Input Layer Size
+        self.output_size = 1 # Fixed Output Layer Size
+        self.hidden_size = 3 # Fixed Hidden Layer Size
         
-        # Weights
+        # Initialize Weights
         self.W1 = np.random.randn(self.input_size, self.hidden_size)
         self.W2 = np.random.randn(self.hidden_size, self.output_size)        
     
     def feedforward(self, X):
-        """ Rerwirte feedforward Tasks: 
-            - Recursive style
-            - Generator style
+        """ Return values of the output layer.
+        Keyword arguments:
+            X -- numpy.array
         """ 
         self.z = np.dot(X, self.W1)
         self.z2 = self.sigmoid(self.z)
@@ -22,19 +23,28 @@ class NeuralNetwork:
     
     
     def sigmoid(self, s, deriv=False):
+        """ Return values sigmoid transformation applied to s.
+        Keyword arguments:
+            s -- numpy.array
+            deriv -- boolean (default False reciprocal if True)
+        """ 
         return s*(1-s) if deriv == True else 1/(1+np.exp(-s))
     
     def backpropagate(self, X, y, output):
-        """ Backpropagation Algorithm """
-        
-        self.output_error = y -output
+        """ Backrpopagation update Weights (minimize the loss function) in each layer.
+        Keyword arguments:
+            X -- numpy.array, inputs values
+            y -- numpy.array, observed output value
+            output -- numpy.array, predicted output
+        """ 
+        self.output_error = y-output
         self.output_delta = self.output_error * self.sigmoid(output, True)
         
         self.z2_error = self.output_delta.dot(self.W2.T)
-        self.z2_delta = self.z2_error * self.sigmoid(self.z2_error, True)
+        self.z2_delta = self.z2_error * self.sigmoid(self.z2, True)
         
         
-        # Update Weights
+        # Update Weight
         self.W1 += X.T.dot(self.z2_delta)
         self.W2 += self.z2.T.dot(self.output_delta)
     
@@ -42,5 +52,3 @@ class NeuralNetwork:
         output = self.feedforward(X)
         self.backpropagate(X, y, output)
         
-        
-        # Compute
--- neural-network/backpropagation/nn-backproba-demo3.py
+++ neural-network/backpropagation/nn-backproba-demo3.py
@@ -1,37 +1,43 @@
-
 import numpy as np
+
 class NeuralNetwork:
     
     def __init__(self, nn_depth, input_size, output_size):
-        self.input_size =  input_size # Input size
+        """ Collecting neural network structure details."""
+        self.input_size =  input_size ## Input size
         self.output_size = output_size # Output size
         self.neurones_per_lay = nn_depth # Hidden Layer size
         
-        # ANN structure
+        # Additional info
         self.nn_shape = [self.input_size] + self.neurones_per_lay +  [self.output_size] 
+        self.depth = len(self.nn_shape)
+        
+        
         self.WEIGHTS = self.generate_weights()
-    
+        # Compute contribution recursively 
+        self.contrib = lambda X, idx: nn.sigmoid(np.dot(X, nn.WEIGHTS[f'w{idx}']))
+        #contrib(contrib(contrib(contrib(contrib(x,0),1),2),3),4)
+
+        
     def generate_weights(self):
         """ Return dictionary of weights corresponding to each layer.""" 
-        return {'w'+str(idx): np.random.randn(*size) for idx, size in enumerate(zip(self.nn_shape[:-1],self.nn_shape[1:]))}        
+        return {f'w{idx-1}': np.random.randn(*(self.nn_shape[idx-1], self.nn_shape[idx])) for idx in range(1, self.depth)}
     
-    def feedforward(self, X):
-        """ Return output layer values & conribution values to the prediction.
+
+    def forward(self, X, l_idx):
+        """ Return conribution values (recursively) to the output at a layer index.
+        Keyword arguments:
+            X -- numpy.array
+        """
+        return self.contrib(self.forward(X,l_idx-1),l_idx) if l_idx else self.contrib(X,0)
+
+    def forward_out_contrib(self, X):
+        """ Return contributions of each layer output.
         Keyword arguments:
             X -- numpy.array
         """ 
-        z_i = [X,]
-        for w_idx in range(len(self.nn_shape)-1):
-            if w_idx ==0:
-                
-                #print(self.WEIGHTS['w'+str(w_idx)])
-                z = self.sigmoid(np.dot(X, self.WEIGHTS[f'w{w_idx}']))
-                print(z)
-                z_i.append(z)
-            else:
-                z = self.sigmoid(np.dot(z, self.WEIGHTS[f'w{w_idx}']))
-                z_i.append(z)
-        return z, z_i # output Z_i, [sigma(X,w0), ... , sigma(z_{l-1}, w{l-1}), output]
+        return [X] + [self.forward(X,i) for i in range(self.depth-1)]
+
     
     def sigmoid(self, s, deriv=False):
         """ Return values sigmoid transformation applied to s.
@@ -40,36 +46,43 @@ class NeuralNetwork:
             deriv -- boolean (default False reciprocal if True)
         """ 
         return s*(1-s) if deriv == True else 1/(1+np.exp(-s))
-    
+
     def backpropagate(self, X, y):
         """ Backrpopagation update Weights (minimize the loss function) in each layer.
         Keyword arguments:
             X -- numpy.array, inputs values
             y -- numpy.array, observed output value
+        TODO:
+            - Backpropagate error recursively
+            - Update Weights recursively
+            - Eliminate for loops
+            - Use generators
         """ 
-        output, z_i = self.feedforward(X)
+        *z_i, output = self.forward_out_contrib(X)
+        print(z_i, output)
+        deltas = []
 
-        deltas = [] # deltas values 
-
-        # Initialuze backward function
+        # First Iteration
         output_error = y - output
         deltas.append(output_error * nn.sigmoid(output, True))
 
-        # Layer reversed index
+        # Backward Iteration
         lay_depth = len(self.nn_shape)
         lay_idx_reversed = [ lay_depth-i -1 for i in range(1, lay_depth)]
 
-        # compute weight & deltas for each Weight & layer
+        # Run Recursevly in inverse from output layer into input layer
         for lay_idx in lay_idx_reversed[:-1]:
             print(f"Layer {lay_idx} ")
             z_error = deltas[-1].dot(self.WEIGHTS[f'w{lay_idx}'].T)        
 
             deltas.append(z_error * self.sigmoid(z_i[lay_idx], True))
 
-        # Updating weights
+        # Output_delta, z_{l-1}, ... , z_1
+        #print("Before Updating Weights: " , self.WEIGHTS)
+
         for idx_rev, idx_frw in zip(lay_idx_reversed, range(lay_depth)):
-            #print(f"Layer Index {idx_rev} Output -> Input direction")
-            #print(f"Layer Index {idx_frw} Input -> Output direction")
+            print(f"Layer Index {idx_rev} Output -> Input direction")
+            print(f"Layer Index {idx_frw} Input -> Output direction")
 
             # Update Weights
             self.WEIGHTS[f'w{idx_frw}'] += z_i[idx_frw].T.dot(deltas[idx_rev])
@@ -78,6 +91,7 @@ class NeuralNetwork:
             #print(idx)
             self.WEIGHTS[f"w{idx}"] += z_i[idx].T.dot(deltas[len(deltas)-1-idx])
 
+        #print("After Updating Weights: " , self.WEIGHTS)
 
     def train(self, X,y, n_iter=100):
         i=0
